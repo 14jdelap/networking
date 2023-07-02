7f62e773d328c19947c22415da6f21568cb30850
José Agustín de la Puente <14jdelap@gmail.com>
2023-07-02 23:42:54 +0100

    Small cleanup

--- shout_server/main.go
+++ shout_server/main.go
@@ -11,28 +11,32 @@ const (
 )
 
 var (
-	address = [4]byte{127, 0, 0, 1}
+	serverAddress = [4]byte{127, 0, 0, 1}
 )
 
 func main() {
+	// Create the server's socket
 	fd, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_DGRAM, PROTOCOL)
 	if err != nil {
 		panic(err)
 	}
 
+	// Bind the socket to an IPv4 address and a port through which to listen to
 	syscall.Bind(fd, &syscall.SockaddrInet4{
 		Port: SERVER_PORT,
-		Addr: address,
+		Addr: serverAddress,
 	})
 
 	defer syscall.Close(fd)
 	for {
+		// Instantiate a 1024 byte slice and copy incoming data into the slice
 		b := make([]byte, 1024)
 		n, from, err := syscall.Recvfrom(fd, b, 0)
 		if err != nil {
 			return
 		}
 		if from != nil {
+			// Uppercase all characters and send the resulting []byte to the client
 			err = syscall.Sendto(fd, bytes.ToUpper(b[:n]), 0, from)
 			if err != nil {
 				return
